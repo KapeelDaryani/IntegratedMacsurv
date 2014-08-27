1ef418bdf2558bd69586b634a8288a458ba76041
KapeelDaryani <kapeel_daryani@hotmail.com>
2014-08-26 19:15:49 -0500

    Manually Controlled Surveillance Vehicle
    Its a piece of code used to take input from user and maneuver the
    modified toy car using WiFi Technology.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace macsurvControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.fpsLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sendSckDebuglbl = new System.Windows.Forms.Label();
+            this.linkBtn = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.rightBtn = new System.Windows.Forms.Button();
+            this.leftBtn = new System.Windows.Forms.Button();
+            this.downBtn = new System.Windows.Forms.Button();
+            this.upBtn = new System.Windows.Forms.Button();
+            this.statusStrip.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fpsLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 430);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // fpsLabel
+            // 
+            this.fpsLabel.AutoSize = false;
+            this.fpsLabel.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.fpsLabel.BackgroundImage = global::macsurvControl.Properties.Resources.camoMacsurv;
+            this.fpsLabel.BorderStyle = System.Windows.Forms.Border3DStyle.Adjust;
+            this.fpsLabel.Name = "fpsLabel";
+            this.fpsLabel.Size = new System.Drawing.Size(609, 17);
+            this.fpsLabel.Spring = true;
+            this.fpsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.videoSourcePlayer.BorderColor = System.Drawing.Color.Transparent;
+            this.videoSourcePlayer.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(4, 3);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer.TabIndex = 0;
+            this.videoSourcePlayer.TabStop = false;
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Controls.Add(this.videoSourcePlayer);
+            this.mainPanel.Location = new System.Drawing.Point(150, 12);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(330, 248);
+            this.mainPanel.TabIndex = 2;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl.Location = new System.Drawing.Point(221, 413);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 15);
+            this.lbl.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // sendSckDebuglbl
+            // 
+            this.sendSckDebuglbl.AutoSize = true;
+            this.sendSckDebuglbl.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.sendSckDebuglbl.Location = new System.Drawing.Point(12, 118);
+            this.sendSckDebuglbl.Name = "sendSckDebuglbl";
+            this.sendSckDebuglbl.Size = new System.Drawing.Size(33, 13);
+            this.sendSckDebuglbl.TabIndex = 21;
+            this.sendSckDebuglbl.Text = "alpha";
+            this.sendSckDebuglbl.Visible = false;
+            // 
+            // linkBtn
+            // 
+            this.linkBtn.Image = global::macsurvControl.Properties.Resources.connect;
+            this.linkBtn.Location = new System.Drawing.Point(12, 15);
+            this.linkBtn.Name = "linkBtn";
+            this.linkBtn.Size = new System.Drawing.Size(60, 60);
+            this.linkBtn.TabIndex = 27;
+            this.linkBtn.UseVisualStyleBackColor = true;
+            this.linkBtn.Click += new System.EventHandler(this.linkBtn_Click_2);
+            // 
+            // button4
+            // 
+            this.button4.Image = global::macsurvControl.Properties.Resources.horn;
+            this.button4.Location = new System.Drawing.Point(420, 288);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 60);
+            this.button4.TabIndex = 26;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // rightBtn
+            // 
+            this.rightBtn.Image = global::macsurvControl.Properties.Resources.rightBtn;
+            this.rightBtn.Location = new System.Drawing.Point(347, 352);
+            this.rightBtn.Name = "rightBtn";
+            this.rightBtn.Size = new System.Drawing.Size(60, 60);
+            this.rightBtn.TabIndex = 25;
+            this.rightBtn.UseVisualStyleBackColor = true;
+            // 
+            // leftBtn
+            // 
+            this.leftBtn.Image = global::macsurvControl.Properties.Resources.leftBtn;
+            this.leftBtn.Location = new System.Drawing.Point(215, 352);
+            this.leftBtn.Name = "leftBtn";
+            this.leftBtn.Size = new System.Drawing.Size(60, 60);
+            this.leftBtn.TabIndex = 24;
+            this.leftBtn.UseVisualStyleBackColor = true;
+            // 
+            // downBtn
+            // 
+            this.downBtn.Image = global::macsurvControl.Properties.Resources.downBtn;
+            this.downBtn.Location = new System.Drawing.Point(281, 352);
+            this.downBtn.Name = "downBtn";
+            this.downBtn.Size = new System.Drawing.Size(60, 60);
+            this.downBtn.TabIndex = 23;
+            this.downBtn.UseVisualStyleBackColor = true;
+            // 
+            // upBtn
+            // 
+            this.upBtn.Image = global::macsurvControl.Properties.Resources.upBtn;
+            this.upBtn.Location = new System.Drawing.Point(281, 288);
+            this.upBtn.Name = "upBtn";
+            this.upBtn.Size = new System.Drawing.Size(60, 60);
+            this.upBtn.TabIndex = 22;
+            this.upBtn.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = global::macsurvControl.Properties.Resources.camoMacsurv;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(624, 452);
+            this.Controls.Add(this.linkBtn);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.rightBtn);
+            this.Controls.Add(this.leftBtn);
+            this.Controls.Add(this.downBtn);
+            this.Controls.Add(this.upBtn);
+            this.Controls.Add(this.sendSckDebuglbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.statusStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MACSURV";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripStatusLabel fpsLabel;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label sendSckDebuglbl;
+        private System.Windows.Forms.Button upBtn;
+        private System.Windows.Forms.Button downBtn;
+        private System.Windows.Forms.Button leftBtn;
+        private System.Windows.Forms.Button rightBtn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button linkBtn;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace macsurvControl
+{
+    public partial class MainForm : Form
+    {
+        private Stopwatch stopWatch = null;
+        public string direction;
+        //public int tTime;
+        public string address;
+        public int upStatus, downStatus, rightStatus, leftStatus, hornStatus;
+        public int output;
+        //static Socket m_sockListener;
+        public string RetPage;
+
+
+        TcpClient client = new TcpClient();
+        IPEndPoint serverEndPoint;
+
+        // Class constructor
+        public MainForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+          protected override void OnKeyDown(KeyEventArgs keyEvent)
+        {
+            keyEvent.Handled = true;
+            if (keyEvent.KeyCode == Keys.W || keyEvent.KeyCode == Keys.NumPad8)
+            {
+                upBtn.BackgroundImage = global::macsurvControl.Properties.Resources.pressed;
+                // Gets the key code; A, W, D 
+                //lbl.Text = "KeyCode: " + keyEvent.KeyCode.ToString();
+                upStatus = 1;
+            }
+            else if (keyEvent.KeyCode == Keys.Space)
+            {
+                downBtn.BackgroundImage = global::macsurvControl.Properties.Resources.pressed;
+               // lbl.Text = "KeyCode: " + keyEvent.KeyCode.ToString();
+                downStatus = 2;
+            }
+            else if (keyEvent.KeyCode == Keys.A || keyEvent.KeyCode == Keys.NumPad4)
+            {
+                leftBtn.BackgroundImage = global::macsurvControl.Properties.Resources.pressed;
+               // lbl.Text = "KeyCode: " + keyEvent.KeyCode.ToString();
+                leftStatus = 8;
+            }
+            else if (keyEvent.KeyCode == Keys.D || keyEvent.KeyCode == Keys.NumPad6)
+            {
+                rightBtn.BackgroundImage = global::macsurvControl.Properties.Resources.pressed;
+               // lbl.Text = "KeyCode: " + keyEvent.KeyCode.ToString();
+                rightStatus = 4;
+            }
+        }
+
+        protected override void OnKeyUp(KeyEventArgs keyEvent)
+        {
+            keyEvent.Handled = true;
+            if (Keys.W == keyEvent.KeyCode || keyEvent.KeyCode == Keys.NumPad8)//Gets the key code
+            {
+                upBtn.BackgroundImage = global::macsurvControl.Properties.Resources.unpressed;
+                upStatus = 0; 
+            }
+            else if (Keys.Space == keyEvent.KeyCode)
+            {
+                downBtn.BackgroundImage = global::macsurvControl.Properties.Resources.unpressed;
+                downStatus = 0;
+            }
+            else if (Keys.D == keyEvent.KeyCode || keyEvent.KeyCode == Keys.NumPad6)
+            {
+                rightBtn.BackgroundImage = global::macsurvControl.Properties.Resources.unpressed;
+                rightStatus = 0;
+            }
+            else if (Keys.A == keyEvent.KeyCode || keyEvent.KeyCode == Keys.NumPad4)
+            {
+                leftBtn.BackgroundImage = global::macsurvControl.Properties.Resources.unpressed;
+                leftStatus = 0;
+            }
+        }
+        
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //// create video source
+            //MJPEGStream mjpegSource = new MJPEGStream("http://192.168.1.191:81/videostream.cgi?user=admin&pwd=admin&resolution=8&rate=11");
+            //// open it
+            //OpenVideoSource(mjpegSource);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+        // "Exit" menu item clicked
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Open video source
+        private void OpenVideoSource(IVideoSource source)
+        {
+            // set busy cursor
+            this.Cursor = Cursors.WaitCursor;
+
+            // stop current video source
+            CloseCurrentVideoSource();
+
+            // start new video source
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start();
+
+            // reset stop watch
+            stopWatch = null;
+
+            // start timer
+            timer.Start();
+
+            this.Cursor = Cursors.Default;
+        }
+
+        // Close video source if it is running
+        private void CloseCurrentVideoSource()
+        {
+            if (videoSourcePlayer.VideoSource != null)
+            {
+                videoSourcePlayer.SignalToStop();
+
+                // wait ~ 3 seconds
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoSourcePlayer.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (videoSourcePlayer.IsRunning)
+                {
+                    videoSourcePlayer.Stop();
+                }
+
+                videoSourcePlayer.VideoSource = null;
+            }
+        }
+
+        // New frame received by the player
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            DateTime now = DateTime.Now;
+            Graphics g = Graphics.FromImage(image);
+
+            // paint current time
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.DrawString(now.ToString(), this.Font, brush, new PointF(5, 5));
+            brush.Dispose();
+
+            g.Dispose();
+        }
+
+        // On timer event - gather statistics
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // call motion in timer to keep the tyres rolling until keys are pressed.
+           motion(upStatus + downStatus + rightStatus + leftStatus + hornStatus);
+        }
+
+        public void motion(int output)
+        {
+            if (client.Connected)
+            {
+                lblIsConn.Text = "Connected";
+                lblNotify.Text = "Sending data";
+                if (output == 0xC || output == 0x1C)
+                {
+                    output = output - 8;
+                }
+                if (output == 0x3 || output == 0x13)
+                {
+                    output = output - 2;
+                }
+                output += 1;
+                NetworkStream clientStream = client.GetStream();
+                byte[] buffer = Encoding.Default.GetBytes(new char[] { (char)output, (char)0 });
+                clientStream.Write(buffer, 0, buffer.Length);
+                //// show me watcha sending in debug window
+                //Debug.WriteLine(Convert.ToString(buffer[1]) + Convert.ToString(buffer[0]));
+                lblNotify.Text = Convert.ToString(buffer[1]) + Convert.ToString(buffer[0]);
+                lblNotify.Text = "";
+                clientStream.Flush();
+            }
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {            // create video source
+            MJPEGStream mjpegSource = new MJPEGStream("http://192.168.1.191:81/videostream.cgi?user=admin&pwd=admin&resolution=8&rate=11");
+            // open it
+            OpenVideoSource(mjpegSource);
+
+            lblNotify.Text = "";
+            try
+            {
+                serverEndPoint = new IPEndPoint(IPAddress.Parse(txtIp.Text), 1500);
+                client.Connect(serverEndPoint);
+                lblIsConn.Text = "Connection Successful";
+            }
+            catch(Exception ex)
+            {
+                lblNotify.Text = "ERROR:" + ex.Message ;
+            }
+        }
+    }
+}
